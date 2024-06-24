eb1022efd88da1c281029d1f9ac14f7ac4a7435b
Cr-Israel <carlosisrael08@hotmail.com>
2024-06-24 15:49:35 -0300

    feat: add error handler global

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,23 @@
 import fastify from "fastify";
 
 import { userRoutes } from "./http/routes/user-routes";
+import { ZodError } from "zod";
+import { env } from "./env";
 
 export const app = fastify()
 
 app.register(userRoutes)
+
+app.setErrorHandler((error, _, reply) => {
+  if (error instanceof ZodError) {
+    return reply
+      .status(400)
+      .send({ message: 'Validantion error.', issues: error.format() })
+  }
+
+  if(env.NODE_ENV !== 'production') {
+    console.error(error)
+  }
+
+  return reply.status(500).send({ message: 'Internal server error.' })
+})
