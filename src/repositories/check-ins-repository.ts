897f23883f6ea536b7552e43b8ac8c765d2fc6cf
Cr-Israel <carlosisrael08@hotmail.com>
2024-07-01 16:10:59 -0300

    feat: add methods to check-ins repository

--- src/repositories/check-ins-repository.ts
+++ src/repositories/check-ins-repository.ts
@@ -1,6 +1,10 @@
 import { CheCkIn, Prisma } from "@prisma/client";
 
 export interface CheckInsRepository {
-  create(data: Prisma.CheCkInUncheckedCreateInput): Promise<CheCkIn>
+  findById(id: string): Promise<CheCkIn | null>
   findByUserIdOnDate(userId: string, date: Date): Promise<CheCkIn | null>
+  findManyByUserId(userId: string, page: number): Promise<CheCkIn[]>
+  countByUserId(userId: string): Promise<number>
+  create(data: Prisma.CheCkInUncheckedCreateInput): Promise<CheCkIn>
+  save(checkIn: CheCkIn): Promise<CheCkIn>
 }
