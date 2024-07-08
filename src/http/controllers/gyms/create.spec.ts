4ba0637461bab73e928f234cc019298d525ef2c8
Cr-Israel <carlosisrael08@hotmail.com>
2024-07-08 17:24:44 -0300

    feat: create a gym test file

---
+++ src/http/controllers/gyms/create.spec.ts
@@ -0,0 +1,32 @@
+import { afterAll, beforeAll, describe, expect, it } from "vitest"
+import request from "supertest"
+
+import { app } from "@/app"
+import { createAndAuthenticateUser } from "@/utils/test/create-and-authenticate-user"
+
+describe('Create Gym (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to create a gym', async () => {
+    const { token } = await createAndAuthenticateUser(app)
+
+    const response = await request(app.server)
+      .post('/gyms')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        title: 'JavaScript Gym',
+        description: 'Some description',
+        phone: '21999999999',
+        latitude: -22.872064,
+        longitude: -43.3160192
+      })
+
+    expect(response.statusCode).toEqual(201)
+  })
+})
