15fbc400347a0ec1f39c58401a327cac7b47deee
Cr-Israel <carlosisrael08@hotmail.com>
2024-07-08 17:26:28 -0300

    feat: user routes

--- src/http/routes/users-routes.ts
+++ src/http/routes/users-routes.ts
@@ -1,12 +1,12 @@
 import { FastifyInstance } from "fastify";
 
-import { register } from "../controllers/register";
-import { authenticate } from "../controllers/authenticate";
-import { profile } from "../controllers/profile";
+import { register } from "../controllers/users/register";
+import { authenticate } from "../controllers/users/authenticate";
+import { profile } from "../controllers/users/profile";
 
 import { verifyJWT } from "../middlewares/verify-jwt";
 
-export async function userRoutes(app: FastifyInstance) {
+export async function usersRoutes(app: FastifyInstance) {
   app.post('/users', register)
   app.post('/sessions', authenticate)
 
