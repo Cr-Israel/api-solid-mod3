f6993c886fe55f35d41a4bcefe01a2d3415785ab
Cr-Israel <carlosisrael08@hotmail.com>
2024-07-09 11:35:35 -0300

    feat: add minddleware verify user role in route of creation gym

--- src/http/routes/gyms-routes.ts
+++ src/http/routes/gyms-routes.ts
@@ -1,6 +1,7 @@
 import { FastifyInstance } from "fastify";
 
 import { verifyJWT } from "../middlewares/verify-jwt";
+import { verifyUserRole } from "../middlewares/verify-user-role";
 
 import { search } from "../controllers/gyms/search";
 import { nearby } from "../controllers/gyms/nearby";
@@ -11,6 +12,6 @@ export async function gymsRoutes(app: FastifyInstance) {
 
   app.get('/gyms/search', search)
   app.get('/gyms/nearby', nearby)
-  
-  app.post('/gyms', create)
+
+  app.post('/gyms', { onRequest: [verifyUserRole('ADMIN')] }, create)
 }
