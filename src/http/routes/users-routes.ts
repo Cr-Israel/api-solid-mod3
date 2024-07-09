0a3bad57c75bace67eda4d1f8182b58a3ebca38c
Cr-Israel <carlosisrael08@hotmail.com>
2024-07-09 10:53:57 -0300

    feat: add patch rote to refresh token

--- src/http/routes/users-routes.ts
+++ src/http/routes/users-routes.ts
@@ -5,10 +5,13 @@ import { authenticate } from "../controllers/users/authenticate";
 import { profile } from "../controllers/users/profile";
 
 import { verifyJWT } from "../middlewares/verify-jwt";
+import { refresh } from "../controllers/users/refresh";
 
 export async function usersRoutes(app: FastifyInstance) {
   app.post('/users', register)
   app.post('/sessions', authenticate)
+
+  app.patch('/token/refresh', refresh)
 
   /* Authenticated */
   app.get('/me', { onRequest: [verifyJWT] }, profile)
